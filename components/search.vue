21be2ee551890bbd8aedd6c1fdf3739044e1c504
Sofía Rivas <sofi.rivasm@gmail.com>
2017-09-22 22:16:32 -0500

    Use current algolia index for reports

--- components/search.vue
+++ components/search.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <ais-index app-id="latency" api-key="3d9875e51fbd20c7754e65422f7ce5e1" index-name="bestbuy" :auto-search="false">    
+  <ais-index app-id="VUPR3RSXRE" api-key="9b7b9defa421ae1ee8b39d8ba9b3f119" index-name="reports" :auto-search="false">
     <ais-search-box>
       <div>
         <h2 class="search-box">Busca información de mascotas rescatadas:</h2>
@@ -9,16 +9,16 @@
         :class-names="{
           'ais-input': 'input'
           }"></ais-input>
-        <span>
+<!--         <span>
           <button class="button is-danger submit-button" type="submit">
             <span class="" aria-hidden="true">Buscar</span>
           </button>
-        </span>
+        </span> -->
       </div>
     </ais-search-box>
     <ais-results>
       <template scope="{ result }">
-        <h2>{{ result.name }}</h2>
+        <h2>{{ result.details.title }}</h2>
       </template>
     </ais-results>
   </ais-index>
