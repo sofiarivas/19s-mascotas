499cdc852f7308123e72a2b10c23504861004e71
Sofía Rivas <sofi.rivasm@gmail.com>
2017-09-24 14:46:28 -0500

    Fix conditional values on layout

--- pages/reports/_id.vue
+++ pages/reports/_id.vue
@@ -14,28 +14,28 @@
         <article class="tile is-child box content">
           <h2 class="is-large">{{ details.title }}</h2>
           <p>{{ details.description }}</p>
-          <div>
+          <div v-if="details.neighborhood">
             <h3>Rescatado en:</h3>
             <ul>
               <li>Zona: {{ details.neighborhood }}</li>
-              <li>Calle: {{ details.street }}</li>     
+              <li v-if="details.street">Calle: {{ details.street }}</li>
             </ul>
           </div>
           <div class="tag-container">
             <h3>Algunos detalles:</h3>
-            <div class="tags">
+            <div v-if="details.pet_details.pet_type" class="tags">
               <span class="tag is-dark">{{ details.pet_details.pet_type }}</span>
               <span class="tag is-light"> {{ details.pet_details.pet_size }}</span>
-              <span class="tag is-light"> {{ details.pet_details.pet_color }}</span>
+              <span v-if="details.pet_details.pet_color" class="tag is-light"> {{ details.pet_details.pet_color }}</span>
             </div>
           </div>
-          <h3>Fecha de reporte: <span class="report-date">{{ details.date }} </span></h3>
+          <h3 v-if="details.date">Fecha de reporte: <span class="report-date">{{ details.date }} </span></h3>
           
         </article>
         <article class="tile is-child box content">
           <h2> Datos de Contacto </h2>
           <h3>Rescatado por: <span class="report-details-text">{{ details.contact_name }}</span></h3>
-          <h3>Detalles de contacto: <span class="report-details-text">{{ details.contact_details }}</span></h3>
+          <h3 v-if="details.contact_details">Detalles de contacto: <span class="report-details-text">{{ details.contact_details }}</span></h3>
           <span class="icon has-text-info">
             <i class="fa fa-phone"></i>
           </span><span class="phone-number">{{ details.contact_number }}</span>
