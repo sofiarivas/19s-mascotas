fa6f0f12ab31acadbe7cbe29b2595263a425cf06
Sofía Rivas <sofi.rivasm@gmail.com>
2017-09-23 23:36:39 -0500

    Add single report page

---
+++ pages/reports/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3> Detalle </h3>
+</template>
+<script>
+import firebase from 'firebase'
+
+export default {
+  asyncData (context) {
+    return { project:
+      firebase.database().ref(`/reports/${context.route.path}`).once('value').then(function (snapshot) {
+        const details = snapshot.val().details
+        console.log(details)
+      })
+    }
+  }
+}
+</script>
